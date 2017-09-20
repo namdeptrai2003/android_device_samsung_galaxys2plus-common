9956e23cf63d4093d94b7867d4ceed0f89ee834c
Real man born on semtember <hieunam03@gmail.com>
2017-09-20 14:40:27 +0700

    Disable sepolicy

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -123,4 +123,4 @@ ANDROID_NO_TEST_CHECK := true
 
 
 # SELinux
-BOARD_SEPOLICY_DIRS += device/samsung/galaxys2plus-common/sepolicy
+#$BOARD_SEPOLICY_DIRS += device/samsung/galaxys2plus-common/sepolicy
